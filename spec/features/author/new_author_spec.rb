f9c2755a32181dbcce3f42e73391b5bfa3d090a4
Christoph Matthies <chrisma@users.noreply.github.com>
2021-11-04 10:14:06 +0100

    Update README with Rail 6.0 info

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 # describe "New author page", type: :feature do
-#   it "should render withour error" do
+#   it "should exist at 'new_author_path' and render withour error" do
+#     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
 #     visit new_author_path
 #   end
 # end
